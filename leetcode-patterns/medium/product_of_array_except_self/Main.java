3fc1d87454004070945772862523f1bdc60af1ce
vl.bondarev <vl.bondarev@qiwi.com>
2022-12-12 16:00:33 +0300

    add medium task

--- leetcode-patterns/medium/product_of_array_except_self/Main.java
+++ leetcode-patterns/medium/product_of_array_except_self/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import static product_of_array_except_self.Solution.productExceptSelf;
 
+// https://leetcode.com/problems/product-of-array-except-self/description/
 public class Main {
     public static void main(String[] args) {
         int[] nums = new int[]{6, 2, 3, 4};
--- leetcode-patterns/medium/product_of_array_except_self/Solution.java
+++ leetcode-patterns/medium/product_of_array_except_self/Solution.java
@@ -2,6 +2,19 @@ package product_of_array_except_self;
 
 public class Solution {
 
+    // the main idea -
+    // Numbers:     2    3    4     5
+    // Lefts:            2  2*3 2*3*4
+    // Rights:  3*4*5  4*5    5
+
+    // fill empty 1
+
+    // Numbers:     2    3    4     5
+    // Lefts:       1    2  2*3 2*3*4
+    // Rights:  3*4*5  4*5    5
+
+
+    // left we will save in input array but right in variable
     public static int[] productExceptSelf(int[] nums) {
         int[] res = new int[nums.length];
         res[0] = 1;
