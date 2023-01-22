cec2625c07d61f323c58e94b2895f7228cc4f3f3
vl.bondarev <vl.bondarev@qiwi.com>
2023-01-22 22:11:32 +0300

    fix

--- leetcode-patterns/booking/Main.java
+++ leetcode-patterns/booking/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 //budget: 309
 //days_per_city: 2
@@ -20,23 +21,26 @@ public class Main {
             "London", List.of(25, 36, 65, 57, 90, 110),
             "Munich", List.of(67, 2, 10, 18, 36, 70));
 
-        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         int daysPerCity = 2;
         int budget = 309;
         boolean[] visited = new boolean[daysPerCity * hotels.size()];
-        backtrack(daysPerCity, hotels, 0, list, result, 0, List.of("Paris", "London", "Munich"), visited);
+        backtrack(budget, daysPerCity, hotels, 0, list, result, 0, List.of("Paris", "London", "Munich"), visited);
 
 
-        System.out.println(result);
+        System.out.println(result.stream().sorted().collect(Collectors.toList()));
     }
 
 
-    public static void backtrack(int daysPerCity, Map<String, List<Integer>> hotels, int cost, List<Integer> costs,
-                                 List<List<Integer>> result, int index, List<String> cityNames, boolean[] visited) {
+    public static void backtrack(int budget, int daysPerCity, Map<String, List<Integer>> hotels, int cost, List<Integer> costs,
+                                 List<Integer> result, int index, List<String> cityNames, boolean[] visited) {
 
+        if (cost > budget) {
+            return;
+        }
         if (costs.size() == daysPerCity * hotels.size()) {
-            result.add(new ArrayList<>(costs));
+            result.add(cost);
             return;
         }
 
@@ -44,16 +48,18 @@ public class Main {
 
         for (int i = 0; i < hotels.get(s).size(); i += daysPerCity) {
             if (visited[i]) continue;
-
+            int sum = 0;
             for (int j = i; j < i + daysPerCity; j++) {
                 costs.add(hotels.get(s).get(j));
+                sum += hotels.get(s).get(j);
                 visited[j] = true;
             }
 
-            backtrack(daysPerCity, hotels, cost, costs,
+            backtrack(budget, daysPerCity, hotels, cost + sum, costs,
                 result, index + 1, cityNames, visited);
 
             for (int j = i; j < i + daysPerCity; j++) {
+                sum -= costs.get(costs.size() - 1);
                 costs.remove(costs.size() - 1);
                 visited[j] = false;
             }
